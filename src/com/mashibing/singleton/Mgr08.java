5e308624ea7bc4788c5fff483e7846517aa4a8ca
18911533031 <18911533031@163.com>
2021-04-02 09:59:43 +0800

    1、增加枚举单例写法

---
+++ src/com/mashibing/singleton/Mgr08.java
@@ -0,0 +1,14 @@
+package com.mashibing.singleton;
+
+public enum Mgr08 {
+
+    INSTANCE;
+
+    private void m(){}
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(()-> System.out.println(Mgr08.INSTANCE.hashCode())).start();
+        }
+    }
+}
